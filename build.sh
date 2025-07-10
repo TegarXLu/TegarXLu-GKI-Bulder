d15513375816ccbeb69e681881ff6540c5e6b7b9
LoggingNewMemory <albert.wesley.dion@gmail.com>
2025-07-10 14:12:18 +0700

    LTO=Thin (GH Action is bussing bcs of no LTO=Thin)

--- build.sh
+++ build.sh
@@ -145,8 +145,7 @@ fi
 export KBUILD_BUILD_USER="$USER"
 export KBUILD_BUILD_HOST="$HOST"
 export KBUILD_BUILD_TIMESTAMP=$(date)
-BUILD_FLAGS="-j$(nproc --all) ARCH=arm64 LLVM=1 LLVM_IAS=1 O=out CROSS_COMPILE=$CROSS_COMPILE_PREFIX"
-KERNEL_IMAGE="$KSRC/out/arch/arm64/boot/Image"
+BUILD_FLAGS="-j$(nproc --all) ARCH=arm64 LLVM=1 LLVM_IAS=1 LTO=thin O=out CROSS_COMPILE=$CROSS_COMPILE_PREFIX"KERNEL_IMAGE="$KSRC/out/arch/arm64/boot/Image"
 KMI_CHECK="$workdir/scripts/KMI_function_symbols_test.py"
 MODULE_SYMVERS="$KSRC/out/Module.symvers"
 
