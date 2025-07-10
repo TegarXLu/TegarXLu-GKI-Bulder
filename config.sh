f83661f2a59ab2f1a5400bbae81aa58ba4b885a8
LoggingNewMemory <albert.wesley.dion@gmail.com>
2025-07-10 13:42:06 +0700

    Really man? lineage-20.0

--- config.sh
+++ config.sh
@@ -17,7 +17,7 @@ KERNEL_DEFCONFIG="gki_defconfig"
 GKI_RELEASES_REPO="https://github.com/LoggingNewMemory/SuiKernel-Release"
 # Clang
 CLANG_URL="https://github.com/mikeNG/android_prebuilts_clang_kernel_linux-x86_clang-r416183b"
-CLANG_BRANCH="lineage-20"
+CLANG_BRANCH="lineage-20.0"
 # Zip name
 # Format: Kernel_name-Linux_version-Variant-Build_date
 ZIP_NAME="$KERNEL_NAME-KVER-VARIANT-BUILD_DATE.zip"
