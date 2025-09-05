f8691fc101f92f379c24fd8784009d57eb867cc5
TegarXLu <botdimas67@gmail.com>
2025-09-05 20:43:32 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -7,14 +7,14 @@ USER="TegarXLu"
 HOST="OnePiece"
 TIMEZONE="Asia/Jakarta"
 # AnyKernel
-ANYKERNEL_REPO="https://github.com/LoggingNewMemory/SuiKernel-anykernel"
-ANYKERNEL_BRANCH="gki"
+ANYKERNEL_REPO="https://github.com/TegarXLu/AnyKernel3"
+ANYKERNEL_BRANCH="main"
 # Kernel Source
 KERNEL_REPO="https://github.com/TegarXLu/Templar-Kernel-GKI-5.10"
 KERNEL_BRANCH="Templar"
 KERNEL_DEFCONFIG="gki_defconfig"
 # Release repository
-GKI_RELEASES_REPO="https://github.com/LoggingNewMemory/SuiKernel-Release"
+GKI_RELEASES_REPO="https://github.com/TegarXLu/GKI_kernel_Build"
 # Clang
 CLANG_URL="https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/d0e0a3882edb1acc193263ae98fce706e82aca38/clang-r574158.tar.gz"
 CLANG_BRANCH="lineage-21.0"
