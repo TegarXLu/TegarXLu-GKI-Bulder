f19a23bba379f85c4e5c6f2e1501a91e7be37ff9
TegarXLu <botdimas67@gmail.com>
2025-09-05 20:55:45 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -16,8 +16,8 @@ KERNEL_DEFCONFIG="gki_defconfig"
 # Release repository
 GKI_RELEASES_REPO="https://github.com/TegarXLu/GKI_kernel_Build"
 # Clang
-CLANG_URL="https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/d0e0a3882edb1acc193263ae98fce706e82aca38/clang-r574158.tar.gz"
-CLANG_BRANCH="lineage-21.0"
+CLANG_URL="https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+/refs/heads/main/clang-r547379"
+CLANG_BRANCH="main"
 # Zip name
 # Format: Kernel_name-Linux_version-Variant-Build_date
 ZIP_NAME="$KERNEL_NAME-KVER-VARIANT-BUILD_DATE.zip"
